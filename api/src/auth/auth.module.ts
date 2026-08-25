723ab31236f6fb725caa3eb56d4f8cd4be220306
Hacker <romanrus98@mail.ru>
2026-08-25 15:31:42 +0500

    fix import

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '@/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { AuthGuard } from './auth.guard';
 import { SessionsModule } from '@/sessions/sessions.module';
 
 @Module({
