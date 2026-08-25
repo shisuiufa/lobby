5acb18713ffd29f8c85640058d059dcad6333e40
Hacker <romanrus98@mail.ru>
2026-08-25 13:35:13 +0500

    add public routes

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
   }
 
+  @Public()
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     return await this.authService.register(registerDto);
